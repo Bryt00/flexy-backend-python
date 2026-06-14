682cb1a20e72c15478720387d37083634384e7f7
Bryt00 <mrbright63@gmail.com>
2026-06-14 10:05:39 +0000

    staff portal & onesignal push

--- flexy_backend/settings.py
+++ flexy_backend/settings.py
@@ -100,6 +100,7 @@ INSTALLED_APPS += [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'corsheaders.middleware.CorsMiddleware', # Priority for CORS
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',

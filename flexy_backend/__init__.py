f18f9146cb0c90ecc81fd14f99d5cd29e5c548f5
Bryt00 <mrbright63@gmail.com>
2026-07-21 21:52:45 +0000

    Fix GIS migrations when GIS is disabled

--- flexy_backend/__init__.py
+++ flexy_backend/__init__.py
@@ -1,3 +1,11 @@
 from .celery import app as celery_app
 
 __all__ = ('celery_app',)
+
+# Patch BaseDatabaseOperations when GIS is disabled to prevent migrations from crashing on old PointFields
+try:
+    from django.db.backends.base.operations import BaseDatabaseOperations
+    if not hasattr(BaseDatabaseOperations, 'geo_db_type'):
+        BaseDatabaseOperations.geo_db_type = lambda self, field: 'geometry'
+except Exception:
+    pass
